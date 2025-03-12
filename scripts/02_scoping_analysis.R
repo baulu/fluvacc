8571eb25256867a7b44af87bce3223999a31897e
baulu <99120677+baulu@users.noreply.github.com>
2025-03-12 21:05:14 +0100

    add testing for assumptions for linear regression (Beusch-Pagan TEst, QQ-plot of residuals ..)

--- scripts/02_scoping_analysis.R
+++ scripts/02_scoping_analysis.R
@@ -10,6 +10,8 @@ library(stringr)
 library(readr)
 library(scales)
 library(patchwork)
+library(ggpubr)
+library(car)
 
 #load data
 microneut_analysis_raw  <- read.xlsx("processed/microneut_analysis_raw.xlsx")
@@ -243,7 +245,7 @@ microneut_analysis_raw %>%
   select(pat_group, Sampling_number, H1 = FluV_H1_fold, H3 = FluV_H3_fold, Vic = FluV_Vic_fold) %>% 
   pivot_longer(cols = 3:5, names_to = "strain", values_to = "fold") %>%  
   filter(Sampling_number == 2) %>% 
-  select(pat_group, strain, fold) %>% print()
+  select(pat_group, strain, fold) %>% 
   filter(pat_group == "ONK") %>% 
   ggplot(aes(x = factor(strain), y = fold, fill = factor(strain))) +  # Factorize Sampling_number for distinct boxplots
   geom_boxplot(outlier.shape = NA, alpha = 0) +  # Boxplot with transparency
@@ -253,8 +255,8 @@ microneut_analysis_raw %>%
 ### regression nach invers. baseline titer (fold change ln2 transformieren vorher und anschl, 2summieren, vgl.pnk-paper)
 ### adjusten unf auf MEdian baselinetiter (vgl. hirzel neurofilament paper)
 
-#####put together dataset with fold changes and baseline titer for all strains
-#h1
+######put together dataset with fold changes and baseline titer for all strains
+###h1
 h1_base <-  microneut_analysis_raw %>%
     filter(Sampling_number == 1) %>% 
     select(PID, H1_baseline = FluA_H1_ic50)
@@ -266,18 +268,57 @@ h1_fold <-  microneut_analysis_raw %>%
 hi1_foldbase <- h1_base %>% 
   left_join(h1_fold) %>% 
   mutate(H1_fold_log2 = log2(H1_fold))  %>% 
-  mutate(H1_fold_log10 = log(H1_fold))  %>% 
+  mutate(H1_fold_log10 = log10(H1_fold))  %>% 
+  mutate(H1_base_log2 = log2(H1_baseline))  %>% 
+  mutate(H1_base_log10 = log10(H1_baseline))  %>% 
   as_tibble()
 
-hi1_foldbase %>% 
+hi1_linreg_plot_baselog2 <- hi1_foldbase %>% 
+  ggplot(aes(x = H1_base_log2, y = H1_fold_log2)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = TRUE) +  # Add regression line with confidence interval
+  #scale_x_continuous(limits = c(0, 13000)) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +  # Add horizontal line
+  theme_minimal()+
+  stat_cor(method = "pearson", label.x = 6, label.y = -4)  + # Display Pearson's r
+  labs(y = "H1 - log2 Fold-Change", x = NULL) 
+
+hi1_linreg_plot_basecont <- hi1_foldbase %>% 
   ggplot(aes(x = H1_baseline, y = H1_fold_log2)) +
   geom_point() +
   geom_smooth(method = "lm", se = TRUE) +  # Add regression line with confidence interval
   scale_x_continuous(limits = c(0, 13000)) +
   geom_hline(yintercept = 0, linetype = "dashed", color = "red") +  # Add horizontal line
-  theme_minimal()
+  theme_minimal()+
+  stat_cor(method = "pearson", label.x = 6, label.y = -4) + # Display Pearson's r
+  labs(y = NULL, x = NULL) 
 
-#h3
+
+# Perform Pearson correlation tests
+cor_result1 <- cor.test(hi1_foldbase$H1_baseline, hi1_foldbase$H1_fold_log2, method = "pearson")
+cor_result2 <- cor.test(hi1_foldbase$H1_base_log2, hi1_foldbase$H1_fold_log2, method = "pearson")
+
+# Extract values and save in a dataframe
+cor_df <- data.frame(
+  Variable1 = "H1_baseline",
+  Variable2 = "H1_fold_log2",
+  Correlation_Coefficient = cor_result1$estimate,
+  P_Value = cor_result1$p.value
+)
+
+cor_df2 <- data.frame(
+  Variable1 = "H1_base_log2",
+  Variable2 = "H1_fold_log2",
+  Correlation_Coefficient = cor_result2$estimate,
+  P_Value = cor_result1$p.value
+)
+
+cor_comb <- cor_df %>% 
+  rows_append(cor_df2) %>% 
+  print()
+ 
+
+###h3
 h3_base <-  microneut_analysis_raw %>%
   filter(Sampling_number == 1) %>% 
   select(PID, H3_baseline = FluA_H3_ic50)
@@ -289,14 +330,205 @@ h3_fold <-  microneut_analysis_raw %>%
 hi3_foldbase <- h3_base %>% 
   left_join(h3_fold) %>% 
   mutate(H3_fold_log2 = log2(H3_fold))  %>% 
-  mutate(H3_fold_log10 = log(H3_fold))  %>% 
+  mutate(H3_fold_log10 = log10(H3_fold))  %>% 
+  mutate(H3_base_log2 = log2(H3_baseline))  %>% 
+  mutate(H3_base_log10 = log10(H3_baseline))  %>% 
   as_tibble()
 
-hi3_foldbase %>% 
-  ggplot(aes(x = H3_baseline, y = H3_fold_log2)) +
+hi3_linreg_plot_baselog2 <- hi3_foldbase %>% 
+  ggplot(aes(x = H3_base_log2, y = H3_fold_log2)) +
   geom_point() +
   geom_smooth(method = "lm", se = TRUE) +  # Add regression line with confidence interval
   #scale_x_continuous(limits = c(0, 13000)) +
   geom_hline(yintercept = 0, linetype = "dashed", color = "red") +  # Add horizontal line
+  theme_minimal()+
+  stat_cor(method = "pearson", label.x = 6, label.y = -4) + # Display Pearson's r
+  labs(y = "H3 - log2 Fold-Change", x = "log2 Transformed Baseline Titers") 
+  
+hi3_linreg_plot_basecont <- hi3_foldbase %>% 
+  ggplot(aes(x = H3_baseline, y = H3_fold_log2)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = TRUE) +  # Add regression line with confidence interval
+  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +  # Add horizontal line
+  theme_minimal()+
+  stat_cor(method = "pearson", label.x = 6, label.y = -4) + # Display Pearson's r
+  labs(y = NULL, x = "Continuous Baseline Titers") 
+
+# Perform Pearson correlation tests
+corh3_result1 <- cor.test(hi3_foldbase$H3_baseline, hi3_foldbase$H3_fold_log2, method = "pearson")
+corh3_result2 <- cor.test(hi3_foldbase$H3_base_log2, hi3_foldbase$H3_fold_log2, method = "pearson")
+
+# Extract values and save in a dataframe
+corh3_df <- data.frame(
+  Variable1 = "H3_baseline",
+  Variable2 = "H3_fold_log2",
+  Correlation_Coefficient = corh3_result1$estimate,
+  P_Value = corh3_result1$p.value
+)
+
+corh3_df2 <- data.frame(
+  Variable1 = "H3_base_log2",
+  Variable2 = "H3_fold_log2",
+  Correlation_Coefficient = corh3_result2$estimate,
+  P_Value = corh3_result2$p.value
+)
+
+corh3_comb <- corh3_df %>% 
+  rows_append(corh3_df2) %>% 
+  print()
+
+###B
+b_base <-  microneut_analysis_raw %>%
+  filter(Sampling_number == 1) %>% 
+  select(PID, Vic_baseline = FluA_Vic_ic50)
+
+b_fold <-  microneut_analysis_raw %>%
+  filter(Sampling_number == 2) %>% 
+  select(PID, Vic_fold = FluV_Vic_fold)
+
+b_foldbase <- b_base %>% 
+  left_join(b_fold) %>% 
+  mutate(Vic_fold_log2 = log2(Vic_fold))  %>% 
+  mutate(Vic_fold_log10 = log10(Vic_fold))  %>% 
+  mutate(Vic_base_log2 = log2(Vic_baseline))  %>% 
+  mutate(Vic_base_log10 = log10(Vic_baseline))  %>% 
+  as_tibble()
+
+b_linreg_plot_baselog2 <- b_foldbase %>% 
+  ggplot(aes(x = Vic_base_log2, y = Vic_fold_log2)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = TRUE) +  # Add regression line with confidence interval
+  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +  # Add horizontal line
+  theme_minimal()+
+  stat_cor(method = "pearson", label.x = 6, label.y = -4) + # Display Pearson's r
+  labs(y = "B - log2 Fold-Change", x = NULL) 
+  
+b_linreg_plot_basecont <- b_foldbase %>% 
+  ggplot(aes(x = Vic_baseline, y = Vic_fold_log2)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = TRUE) +  # Add regression line with confidence interval
+  scale_x_continuous(limits = c(0, 20000)) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +  # Add horizontal line
+  theme_minimal()+
+  stat_cor(method = "pearson", label.x = 6, label.y = -4) + # Display Pearson's r
+  labs(y = NULL, x = NULL) 
+
+# Perform Pearson correlation tests
+corb_result1 <- cor.test(b_foldbase$Vic_baseline, b_foldbase$Vic_fold_log2, method = "pearson")
+corb_result2 <- cor.test(b_foldbase$Vic_base_log2, b_foldbase$Vic_fold_log2, method = "pearson")
+
+# Extract values and save in a dataframe
+corb_df <- data.frame(
+  Variable1 = "B_baseline",
+  Variable2 = "B_fold_log2",
+  Correlation_Coefficient = corb_result1$estimate,
+  P_Value = corb_result1$p.value
+)
+
+corb_df2 <- data.frame(
+  Variable1 = "B_base_log2",
+  Variable2 = "B_fold_log2",
+  Correlation_Coefficient = corb_result2$estimate,
+  P_Value = corb_result2$p.value
+)
+
+corb_comb <- corb_df %>% 
+  rows_append(corb_df2) %>% 
+  print()
+
+###combining lin. regression plots
+linreg_plot_comb = (b_linreg_plot_baselog2 + b_linreg_plot_basecont) / (hi1_linreg_plot_baselog2 + hi1_linreg_plot_basecont) / (hi3_linreg_plot_baselog2 + hi3_linreg_plot_basecont)
+
+# linear regression of baseline titer only in controls - change baseline-dataset for analysis before
+#microneut_analysis_raw <- microneut_analysis_raw %>% 
+#  filter(pat_group == "Control")
+
+
+#### Checking if homoscedasticity is given (constant scattering of residuals independant of baseline)
+###H3-Strain
+# Remove NAs to ensure matching rows
+h3_foldbase_clean <- hi3_foldbase %>% drop_na(H3_base_log2, H3_fold_log2)
+
+# Fit linear model
+adj_model_h3 <- lm(H3_fold_log2 ~ H3_base_log2, data = h3_foldbase_clean)
+
+# Check for homoscedasticity using residuals vs fitted values plot
+h3_residuals_plot <- ggplot(data = h3_foldbase_clean, aes(x = fitted(adj_model_h3), y = resid(adj_model_h3))) +
+  geom_point() +
+  geom_smooth(method = "loess", se = FALSE, linetype = "dashed") +
+  labs(x = "Fitted Values", y = "Residuals") +
+  #ggtitle("H3 Residuals vs Fitted Values") +
   theme_minimal()
 
+# Perform Breusch-Pagan test to check for homoscedasticity
+durbinWatsonTest(adj_model_h3)
+bp_test_h3 <- ncvTest(adj_model_h3)
+print(bp_test_h3)
+
+# QQ-Plot of residuals from a regression model
+h3_qq_plot <- ggplot(data = data.frame(resid = residuals(adj_model_h3)), aes(sample = resid)) +
+  stat_qq() +
+  stat_qq_line(color = "red") +
+  theme_minimal() +
+  labs(x = "Theoretical Quantiles", y = "Sample Quantiles")
+
+###H3-Strain
+# Remove NAs to ensure matching rows
+h1_foldbase_clean <- hi1_foldbase %>% drop_na(H1_base_log2, H1_fold_log2)
+
+# Fit linear model
+adj_model_h1 <- lm(H1_fold_log2 ~ H1_base_log2, data = h1_foldbase_clean)
+
+# Check for homoscedasticity using residuals vs fitted values plot
+h1_residuals_plot <- ggplot(data = h1_foldbase_clean, aes(x = fitted(adj_model_h1), y = resid(adj_model_h1))) +
+  geom_point() +
+  geom_smooth(method = "loess", se = FALSE, linetype = "dashed") +
+  labs(x = "Fitted Values", y = "Residuals") +
+  #ggtitle("H1 Residuals vs Fitted Values") +
+  theme_minimal()
+
+# Perform Breusch-Pagan test to check for homoscedasticity
+durbinWatsonTest(adj_model_h1)
+bp_test_h1 <- ncvTest(adj_model_h1)
+print(bp_test_h1)
+
+# QQ-Plot of residuals from a regression model
+h1_qq_plot <- ggplot(data = data.frame(resid = residuals(adj_model_h1)), aes(sample = resid)) +
+  stat_qq() +
+  stat_qq_line(color = "red") +
+  theme_minimal() +
+  labs(x = "Theoretical Quantiles", y = "Sample Quantiles")
+
+###B-Strain
+# Remove NAs to ensure matching rows
+b_foldbase_clean <- b_foldbase %>% drop_na(Vic_base_log2, Vic_fold_log2)
+
+# Fit linear model
+adj_model_b <- lm(Vic_fold_log2 ~ Vic_base_log2, data = b_foldbase_clean)
+
+# Check for homoscedasticity using residuals vs fitted values plot
+b_residuals_plot <- ggplot(data = b_foldbase_clean, aes(x = fitted(adj_model_b), y = resid(adj_model_b))) +
+  geom_point() +
+  geom_smooth(method = "loess", se = FALSE, linetype = "dashed") +
+  labs(x = "Fitted Values", y = "Residuals") +
+  ggtitle("Residuals vs Fitted Values") +
+  theme_minimal()
+
+# Perform Breusch-Pagan test to check for homoscedasticity
+durbinWatsonTest(adj_model_b)
+bp_test_b <- ncvTest(adj_model_b)
+print(bp_test_b)
+
+
+# QQ-Plot of residuals from a regression model
+b_qq_plot <- ggplot(data = data.frame(resid = residuals(adj_model_b)), aes(sample = resid)) +
+  stat_qq() +
+  stat_qq_line(color = "red") +
+  theme_minimal() +
+  labs(title = "QQ-Plot of Residuals", x = "Theoretical Quantiles", y = "Sample Quantiles")
+
+
+###combining regression plots
+linreg_plot_test_combined = ((b_linreg_plot_baselog2 + b_residuals_plot + b_qq_plot) /
+                      (hi1_linreg_plot_baselog2 + h1_residuals_plot + h1_qq_plot) / 
+                      (hi3_linreg_plot_baselog2 + h3_residuals_plot + h3_qq_plot))
